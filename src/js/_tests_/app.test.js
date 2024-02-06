b3a2d2ff80a1b6b663ea94872d0e99ad9987ddd4
nikitaSB01 <118895582+nikitaSB01@users.noreply.github.com>
2024-02-06 14:15:18 +0600

    сделал test

--- src/js/_tests_/app.test.js
+++ src/js/_tests_/app.test.js
@@ -1,2 +1,40 @@
 // прописать import все файлы JS
+import orderByProps from '../app';
 // начать тесты
+test('Сортировка вначале по массиву, потом по алфавиту name level', () => {
+  const obj = {
+    name: 'мечник',
+    health: 10,
+    level: 2,
+    attack: 80,
+    defence: 40,
+  };
+  const resFunc = orderByProps(obj, ['name', 'level']);
+  const result = [
+    { key: 'name', value: 'мечник' },
+    { key: 'level', value: 2 },
+    { key: 'attack', value: 80 },
+    { key: 'defence', value: 40 },
+    { key: 'health', value: 10 },
+  ];
+  expect(resFunc).toEqual(result);
+});
+
+test('Сортировка вначале по массиву, потом по алфавиту level', () => {
+  const obj = {
+    name: 'мечник',
+    health: 10,
+    level: 2,
+    attack: 80,
+    defence: 40,
+  };
+  const resFunc = orderByProps(obj, ['level']);
+  const result = [
+    { key: 'level', value: 2 },
+    { key: 'attack', value: 80 },
+    { key: 'defence', value: 40 },
+    { key: 'health', value: 10 },
+    { key: 'name', value: 'мечник' },
+  ];
+  expect(resFunc).toEqual(result);
+});
